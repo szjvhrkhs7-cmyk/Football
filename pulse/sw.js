2c0beba304fc6f232c1d38ba5c4ddc4d5c0a9136
szjvhrkhs7-cmyk <szjvhrkhs7@privaterelay.appleid.com>
2026-09-17 20:30:21 +0300

    Refresh Pulse app shell cache

--- pulse/sw.js
+++ pulse/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'pulse-v1.0.0';
-const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'pulse-v1.0.1';
+const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './ui-language.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
