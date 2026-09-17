458699e7117937c4140206e2a44e5059c899914b
szjvhrkhs7-cmyk <szjvhrkhs7@privaterelay.appleid.com>
2026-09-17 20:34:50 +0300

    Refresh Pulse cache after overview editor update

--- pulse/sw.js
+++ pulse/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pulse-v1.0.1';
+const CACHE = 'pulse-v1.0.2';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './ui-language.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
